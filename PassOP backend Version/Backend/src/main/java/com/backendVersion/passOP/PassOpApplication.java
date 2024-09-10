23fc0a1110e5a849f3c2a008db6a43b644c4a5fb
AnonymousManan <mananaggarwal2001@gmail.com>
2024-09-10 16:10:10 +0530

    changes made in the folder

---
+++ PassOP backend Version/Backend/src/main/java/com/backendVersion/passOP/PassOpApplication.java
@@ -0,0 +1,13 @@
+package com.backendVersion.passOP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PassOpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PassOpApplication.class, args);
+	}
+
+}
---
+++ PassOP backend Version/Backend/src/test/java/com/backendVersion/passOP/PassOpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backendVersion.passOP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PassOpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
